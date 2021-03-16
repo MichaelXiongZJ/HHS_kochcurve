936e163180a6b8e467048fa60ecbb1607735bc14
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-03-15 21:38:08 -0700

    update

--- KochCurve/src/KochCurve.java
+++ KochCurve/src/KochCurve.java
@@ -20,8 +20,21 @@ public class KochCurve {
     	drawKochCurve(/* TO DO */);
     }
 
-    private /* TO DO */ drawKochCurve(/* TO DO */) {
-    	// TO DO
+    private void drawKochCurve(PApplet marker, double length, double angle, double x, double y, int level) {
+    	if(level < 1) {
+        	Draw a straight line of the current length
+    	}else {
+    		Draw a k-1 level Koch curve of 1/3 the current length
+
+        	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
+        	current length, at an angle of 60 degrees with respect to the current angle
+
+        	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
+        	current length, at an angle of -60 degrees with respect to the current angle
+
+        	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
+        	current length
+    	}
     }
 
 }
