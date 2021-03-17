cd2ebad59658b0ff08f894167ca74eb6298a3c9c
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-03-17 14:11:21 -0700

    Update KochCurve.java

--- KochCurve/src/KochCurve.java
+++ KochCurve/src/KochCurve.java
@@ -13,14 +13,23 @@ public class KochCurve {
 
 	// TO DO
 	private Line line;
-	private boolean useAngleLine = true;
+	private boolean useAngleLine;
+	private double xLoc;
+	private double yLoc;
+	private int lvl;
+	private int inL;
+	private int inA;
 	
     public KochCurve(int level, int length) {
-    	// TO DO
+    	lvl = level;
+    	inL = length;
+    	inA = 0;
+    	xLoc = 200;
+    	yLoc = 200;
     }
     
     public void draw(PApplet marker) {
-    	drawKochCurve(marker, 5, 90, 50, 50, 1);
+    	drawKochCurve(marker, inL, inA, xLoc, yLoc, lvl);
     }
 
     private void drawKochCurve(PApplet marker, double length, double angle, double x, double y, int level) {
@@ -30,18 +39,21 @@ public class KochCurve {
     	}else {
     	//	Draw a k-1 level Koch curve of 1/3 the current length
     		drawKochCurve(marker, length/3, angle, x, y, level-1);
-    		
+    		xLoc = line.getX2();
+    		yLoc = line.getY2();
         //	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
         //	current length, at an angle of 60 degrees with respect to the current angle
-    		drawKochCurve(marker, length/3, 60, x, y, level-1);
-    		
+    		drawKochCurve(marker, length/3, angle+60, xLoc, yLoc, level-1);
+    		xLoc = line.getX2();
+    		yLoc = line.getY2();
         //	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
         //	current length, at an angle of -60 degrees with respect to the current angle
-    		drawKochCurve(marker, length/3, -60, x, y, level-1);
-    		
+    		drawKochCurve(marker, length/3, angle-60, xLoc, yLoc, level-1);
+    		xLoc = line.getX2();
+    		yLoc = line.getY2();
         //	Starting where the previous left off, draw a k-1 level Koch curve of 1/3 the
         //	current length
-    		drawKochCurve(marker, length/3, angle, x, y, level-1);
+    		drawKochCurve(marker, length/3, angle, xLoc, yLoc, level-1);
     	}
     }
 
